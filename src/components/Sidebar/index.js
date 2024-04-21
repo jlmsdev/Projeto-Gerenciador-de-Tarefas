0d754aaeb7397ad1ea621bc43ef28e1643f5ddba
jlmsdev <joaosouza1135@gmail.com>
2024-04-20 22:59:49 -0300

    sinalizando qual link esta ativo

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,21 +1,26 @@
 import { Link } from 'react-router-dom';
-import './sidebar.css';
 import { GrTask } from "react-icons/gr";
 import { TbAlertHexagon } from "react-icons/tb";
+import Styles from './sidebar.module.css'
 
 
 
 
 export default function Sidebar() {
+
+    const urlAtual = window.location.href;
+    const urlTarefas = urlAtual.endsWith('tarefas');
+    const urlConcluido = urlAtual.endsWith('TarefasConcluidas');
+
     return(
         <>
-            <section className="containerSidebar">
-                <nav className='nav'>
-                <Link to='/tarefas'>
+            <section className={Styles.containerSidebar}>
+                <nav className={Styles.nav}>
+                <Link to='/tarefas' className={ urlTarefas === true ? Styles.linkAtivo : '' }>
                         <TbAlertHexagon className='iconeTarefa'  size={25} color='#cecece'/>
                         Tarefas Pendentes
                     </Link>
-                    <Link to='/tarefas/TarefasConcluidas'>
+                    <Link to='/tarefas/TarefasConcluidas' className={ urlConcluido === true ? Styles.linkAtivo : '' }>
                         <GrTask className='iconeTarefa'  size={25} color='#cecece'/>
                         Tarefas Concluidas
                     </Link>
